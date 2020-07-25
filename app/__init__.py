faf25f8d96278599d001cca96f4664f40c6b5b23
justanothercsguy <zishiwu123@gmail.com>
2020-07-25 17:41:49 -0400

    feature: user registration and upload to database
    User fills out form with name, email, and password. This gets
    populated into a database table called User that enforces
    referential integrity to make sure no name or email is duplicated.
    Currently we do not validate if the email is a valid email or not.
    
    Along the way, we made many errors and discoveries. For example,
    committing a database transaction on invalid inputs can lead to
    users existing in a database even though the screen says they
    failed to register, so it's very important to put try/except
    statements before committing to database. This prevents situations
    international students applying for work VISA's not being able
    to get into their account because their name and email already
    exists from the first registration page which said registration
    failed, but the commit to the database still occurred.
    
    We haven't implemented the user login yet, but it should be
    similar to registration. We also have not encrpyted the password.

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,79 @@
 from __future__ import print_function
 from flask import Flask, jsonify, render_template
-from flask import url_for, escape
+from flask import escape, request, url_for
 import json
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+from app.models import MyForm
 
 
 app = Flask(__name__, instance_relative_config=True)
-data_path='test/song.json'
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+db = SQLAlchemy(app)
+
+
+# START: Database Tables
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<User: id={self.id}, name={self.name}, email={self.email}>'
+# END: Database Tables
+
+
+# START: run when web app starts up
+data_path = 'test/song.json'
 with open(data_path) as f:
     test_data = json.load(f)
+# END: run when web app starts up
+
+
+# START: Utility functions
+def add_user_to_db(name, email, password) -> bool:
+    """Return `False` if unable to insert entry to User table due to bad input
+    or some referential integrity violation. Otherwise return `True`."""
+    print(f"Trying adding user: {name} with email: {email} to database")
+    try:
+        user = User(name=name, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return True
+    except Exception:
+        return False
+# END: Utility functions #
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = MyForm()
+    return render_template('register.html', form=form)
+
+
+@app.route('/submit', methods=('GET', 'POST'))
+def submit():
+    """Submit user registration form to database and redirect to success/failure status view.
+    """
+    form = MyForm(
+        name=request.form['name'],
+        email=request.form['email'],
+        password=request.form['password']
+    )
+    status = form.validate()
+    if status:
+        status = add_user_to_db(
+            name=request.form['name'],
+            email=request.form['email'],
+            password=request.form['password']
+        )
+    return render_template('/status.html', status=status)
+
 
 @app.route('/')
 @app.route('/home')
@@ -25,21 +91,23 @@ def all_data():
     '''
     return jsonify(test_data)
 
-@app.route('/render_all_data',methods = ['POST','GET'])
+
+@app.route('/render_all_data', methods=['POST', 'GET'])
 def render_all_data():
     '''
     arg: None
     return: a rendered html using the entire json data
     '''
-    ### problem right now: the html page is not being rendered while no error is incurred ###
-    data_path='test/song.json'
+    data_path = 'test/song.json'
     with open(data_path) as f:
         test_data = json.load(f)
-    return render_template('visualize_data.html',data=test_data)
+    return render_template('visualize_data.html', data=test_data)
+
 
 @app.route('/user/<name>')
 def user_page(name):
     return "User: %s" % escape(name)
+
 
 @app.route('/test')
 def test_url_for():
@@ -47,16 +115,9 @@ def test_url_for():
     print(url_for('user_page', name="Haosong"))
     print(url_for('user_page', name="Zishi"))
     print(url_for("test_url_for"))
-    print(url_for('test_url_for',num=2))
+    print(url_for('test_url_for', num=2))
     return "Test page"
-
-
-# will move to utils later
-# def parse_song_data(data):
-#     for artist in data:
-#         print(artist,file=sys.stderr)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
